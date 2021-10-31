611ecd1b4c2340ee295526bae892bf8bb5b7e3ef
rmuehe <rmuehe@gmail.com>
2021-10-30 20:50:59 -0700

    Refactor to object orientation

--- full_name.rb
+++ full_name.rb
@@ -1,14 +1,32 @@
 # Write a program which asks for a person's first name, then middle, then last.  It should store each of these parts in an array. Finally, it should greet the person using their full name.
 
-$names = []
+class Name
+  attr_accessor :first, :middle, :last 
+  def initialize(first, middle, last)
+    @first = first.strip
+    @middle = middle.strip
+    @last = last.strip
+  end
 
-def prompt_name(name_position)
-  print("Enter your #{name_position} name: ")
-  $names.push(gets.chomp())
-end  
+  def to_s
+    name_str = ""
+    [@first, @middle, @last].each do |name|
+      name_str += name + " " unless name == ""  
+    end 
+    name_str.strip
+  end
+end
 
-prompt_name("first")
-prompt_name("middle")
-prompt_name("last")
+def prompt_names(name_array)
+  names = []
+  name_array.each do |name| 
+    print("Enter your #{name} name: ")
+    names.push(gets.chomp)
+  end
+  names  
+end
 
-puts "Hello, #{$names[0]} #{$names[1]} #{$names[2]}"
+full_name_arr = ["first", "middle", "last"]
+names = prompt_names(full_name_arr)
+full_name = Name.new(*names)
+puts "Hello, " + full_name.to_s
