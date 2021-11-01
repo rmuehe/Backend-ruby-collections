4639ffc50ab1ddadbe39a504252274f4d01c519d
rmuehe <rmuehe@gmail.com>
2021-10-31 20:58:25 -0700

    Clarify method output & add a test

--- add_up.rb
+++ add_up.rb
@@ -1,5 +1,6 @@
 # Write a program with a function add_up(i) .  It is to be passed a positive integer, and it will add all the numbers from 1 to that integer and return the sum.  Call the function three times within the program, and each time print out the return value.
 
+# returns the sum of integers from 1 to i given a positive int
 def add_up(i)
   sum = 0
   while i >= 1
@@ -9,10 +10,46 @@ def add_up(i)
   sum  
 end
 
-def add_and_print(num)
-  puts add_up(num)
+# returns a '+' separated integer range string 
+def add_up_str(i)
+  int_arr = []
+  integer = 0
+  until i < 1
+    integer += 1
+    int_arr << integer
+    i -= 1
+  end 
+  add_str = int_arr.join(" + ")  
+end  
+
+# returns the sum of integers from 1 to num, 
+# where num is a random integer from 1-10 by default.
+# prints out the math
+def random_sum(num = 10)
+  random = rand(num) + 1
+  # TODO: clean up this string
+  puts "The sum of all positive integers inclusive to #{random}: \n#{add_up_str(random)}\n= #{add_up(random)}\n "
+  
+  add_up(random)
 end
 
-add_and_print(1)
-add_and_print(2)
-add_and_print(3)
+puts
+# prints 3 random integer sums from: 
+# [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+3.times {random_sum}
+
+# prints out a test for the add_up method
+def add_up_test(num = 10)
+  index = 0
+  array = []
+  until index > num
+    puts "i is: #{index}"
+    puts add_up(index)
+    array << add_up(index)
+    index += 1
+  end
+  print(array, "\n")
+  array
+end 
+
+# add_up_test 5
